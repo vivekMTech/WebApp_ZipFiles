86cfbfcb30eb8c5bca5284e5e1c289104c9f7c5a
vivekMTech <vivek.parmar@marutitech.com>
2022-01-31 10:08:26 +0530

    files updated.

--- WebApp_ZipFiles/main.py
+++ WebApp_ZipFiles/main.py
@@ -1,51 +1,47 @@
-from flask import Flask, render_template, flash, send_from_directory
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileRequired
-from wtforms import FileField
-from zipfile import ZipFile
-import shutil
+from flask import Flask, render_template, flash, send_file  # import flask and their classes
+from flask_wtf import FlaskForm  # flask form for uploading zip files
+from flask_wtf.file import FileAllowed, FileRequired  # allowed files
+from wtforms import FileField  # file field for uploading files
+from zipfile import ZipFile  # extract zip files
+import shutil  # removing files from pwd
 import os
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+app = Flask(__name__)  # initialize flask app
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'  # secret key for authentication
 
 
-class upload_zip(FlaskForm):
+class upload_zip(FlaskForm):  # class for fileField form
     file = FileField(label="Select file : ",
-                     validators=[FileAllowed(['zip']), FileRequired()])
+                     validators=[FileAllowed(['zip']), FileRequired()])  # validators only allow zip files
 
 
-def extract_zip(file_name, dir_name):
-    shutil.rmtree('extracted_files')
-    with ZipFile(file_name, "r") as zip_ref:
-        zip_ref.extractall(dir_name)
+def extract_zip(file_name, dir_name):  # extract zip method
+    shutil.rmtree('extracted_files')  # removing files from folder
+    with ZipFile(file_name, "r") as zip_ref:  # process of extract files
+        zip_ref.extractall(dir_name)  # extractall() method for extracting files
 
 
-@app.route("/", methods=['GET', 'POST'])
-@app.route('/upload', methods=['GET', 'POST'])
-def uploadRoute():
-    lst = []
-    form = upload_zip()
-    if form.validate_on_submit():
-        zip_file = form.file.data
-        extract_zip(zip_file, 'extracted_files')
-        for files in os.listdir("extracted_files"):
+@app.route("/", methods=['GET', 'POST'])  # index route
+@app.route('/upload', methods=['GET', 'POST'])  # upload route
+def uploadRoute():  # method for uploading zip file
+    lst = []  # lst for files
+    form = upload_zip()  # initialize form object
+    if form.validate_on_submit():  # if data is valid then process further
+        zip_file = form.file.data  # get file in zip_file var
+        extract_zip(zip_file, 'extracted_files')  # extract zip file
+        for files in os.listdir("extracted_files"):  # appending extracted file in lst
             lst.append(files)
-        return render_template("shownZipFiles.html", files=lst)
+        return render_template("shownZipFiles.html", files=lst)  # call "shownZipFiles.html" and send file lst
     else:
-        flash("Please, Select only zip file.", category='danger')
-    return render_template("uploadZip.html", form=form)
+        flash("Please, Select only zip file.", category='danger')  # if file are not validate then print flash message
+    return render_template("uploadZip.html", form=form)  # call "uploadZip.html" with form
 
 
-@app.route("/shown")
-def showRoute():
-    return render_template("shownZipFiles.html")
+@app.route("/<filename>", methods=['GET', 'POST'])  # route for downloading file
+def download_file(filename):  # method for download file
+    file_path = "extracted_files/" + filename  # file path, where file is located
+    return send_file(file_path, as_attachment=True, attachment_filename='')  # send_file() send content to client pc.
 
 
-@app.route('/<path:filename>', methods=['GET', 'POST'])
-def download_file(filename):
-    return send_from_directory('extracted_files', filename)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__':  # init main
+    app.run(debug=True)  # run app in debug mode
