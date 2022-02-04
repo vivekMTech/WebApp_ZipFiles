b035140dbea4fd59544297eeb73b39a2cacde604
vivekMTech <vivek.parmar@marutitech.com>
2022-02-04 23:59:58 +0530

    files updated.

--- WebApp_ZipFiles/main.py
+++ WebApp_ZipFiles/main.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template, flash, send_file  # import flask and t
 from flask_wtf import FlaskForm  # flask form for uploading zip files
 from flask_wtf.file import FileAllowed, FileRequired  # allowed files
 from wtforms import FileField  # file field for uploading files
-import zipfile  # extract zip files
+import patoolib  # for extracting archive files
 import shutil  # removing files from pwd
 from werkzeug.utils import secure_filename  # get file name
 import os
 
 app = Flask(__name__)  # initialize flask app
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'  # secret key for authentication
-global extracted_path, dir_count  # extracted files path & current dir count
-dir_count = 0  # init directory count=0
+global extracted_path
 
 
 class upload_zip(FlaskForm):  # class for fileField form
@@ -18,23 +17,19 @@ class upload_zip(FlaskForm):  # class for fileField form
                      validators=[FileAllowed(['zip', 'rar']), FileRequired()])  # validators only allow zip files
 
 
-def extract_zip(zip_file, filename):  # extract zip method
-    global dir_count
-    dir_count += 1  # increment directory cont
-    child_dir_path = os.path.join("extracted_files", filename + str(dir_count))  # prepare path for sub folder
+def extract_zip(filename):  # extract zip method
+    child_dir_path = os.path.join("extracted_files", filename)  # prepare path for sub folder
     try:
         shutil.rmtree(child_dir_path)  # removing files from sub-folder
     except OSError as error:
         os.makedirs(child_dir_path)  # If folder was not their then it created.
 
-        try:
-            with zipfile.ZipFile(zip_file, "r") as zip_ref:  # process of extract files:
-                zip_ref.extractall(child_dir_path)  # extractall() method for extracting files
-        except zipfile.BadZipfile:
-            flash("Please, Your ZIP or RAR files is Corrupted.", category='danger')
-        except zipfile.LargeZipFile:
-            flash("Please, Your ZIP or RAR files is Large.", category='danger')
-
+    try:
+        patoolib.extract_archive(os.path.join('ZipFiles/', filename), outdir=child_dir_path)  # extract file
+        os.remove(os.path.join('ZipFiles/', filename))  # remove copied zip or rar file
+    except Exception as error: # exception if zip or rar is corrupt
+        flash("Please, Your ZIP or RAR files is Corrupted.", category='danger')
+        print("Exception : {}".format(error))
     return child_dir_path
 
 
@@ -46,7 +41,9 @@ def upload_route():  # method for uploading zip file
     if form.validate_on_submit():  # if data is valid then process further
         zip_file = form.file.data  # get file in zip_file var
         filename = secure_filename(form.file.data.filename)  # get filename
-        extracted_path = extract_zip(zip_file, filename)  # extract zip file
+        os.makedirs('ZipFiles', exist_ok=True)  # make ZipFile directory for storing zip or rar files temp
+        zip_file.save(os.path.join('ZipFiles/', filename))  # save file
+        extracted_path = extract_zip(filename)  # extract zip file
         return render_template("shownZipFiles.html", files=os.listdir(extracted_path))
     else:
         # if file are not validate then print flash message
